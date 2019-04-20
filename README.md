6744b7e4454bb081ceb88e11c3a3f056fafa52a5
Joshua Davis <me@jdav.is>
2019-04-20 09:04:23 -0400

    updated sonar config and readme badges

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # docs.ci
 docs.ci is a generator for various Docs-as-Code documents and can be used standalone or in CI builds.
+
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=a11smiles_docs.ci&metric=alert_status)](https://sonarcloud.io/dashboard?id=a11smiles_docs.ci) [![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE.md) ![Supported Platforms](https://img.shields.io/badge/platforms-win--64%20%7C%20linux--64%20%7C%20osx--64-lightgrey.svg) ![Total Downloads](https://img.shields.io/github/downloads/a11smiles/docs.ci/total.svg?color=ff0090)
+
+| Build server | Platform | Build status | Download |
+|--------------|----------|--------------|----------|
+| Azure Pipelines | Windows | _coming soon_ | ![Download Zip](https://img.shields.io/badge/download-zip-brightgreen.svg) |
+| Azure Pipelines | Ubuntu  | _coming soon_ | _coming soon_|
+| Azure Pipelines | macOS | _coming soon_ | _coming soon_ |
+| AppVeyor | Windows | _coming soon_ | |
+| Travis | Linux | _coming soon_ | |
+| Travis | macOS | _coming soon_ | |
--- sonar.sh
+++ sonar.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 token="$(cat sonar.txt)"
 dir="$(pwd)"
-dotnet sonarscanner begin /k:"a11smiles_docs.ci" /n:"docs.ci" /v:"1.0.0" /o:"a11smiles-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${token}" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*" /d:sonar.coverage.exclusions="test/**" /d:sonar.cs.opencover.reportsPaths="${dir}/lcov.opencover.xml"
-dotnet restore
-dotnet build
-dotnet test Project.Tests/Project.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=../lcov /p:Include="[Docs]*"
+dotnet sonarscanner begin /k:"a11smiles_docs.ci" /n:"docs.ci" /v:"1.0.0" /o:"a11smiles-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${token}" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*" /d:sonar.coverage.exclusions="**/*Test.cs,**/Program.cs" /d:sonar.cs.opencover.reportsPaths="${dir}/.coverage/lcov.opencover.xml"
+dotnet restore src/Docs/Docs.csproj
+dotnet build src/Docs/Docs.csproj
+dotnet restore test/Docs.Tests/Docs.Tests.csproj
+dotnet test Project.Tests/Project.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=../../.coverage/lcov /p:Include="[Docs]*"
 dotnet sonarscanner end /d:sonar.login="${token}"
